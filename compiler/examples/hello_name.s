7ab88967d161d9e7b8371f2989d76d79ce9f8da9
Dru Jensen <drujensen@gmail.com>
2021-06-13 21:28:19 -0700

    terminate strings with 00

--- compiler/examples/hello_name.s
+++ compiler/examples/hello_name.s
@@ -1,10 +1,12 @@
 ; print hello, Dru
 init: LDA# ques ; load direct "ques" address to A
       STA ptr   ; store address to ptr
-loop: OUT* ptr  ; output indirect value at ptr
+loop: LDA ptr   ; load ptr to A
+      OUT* ptr  ; output indirect value at ptr
       INC       ; increment ptr
       STA ptr   ; store new position to ptr
-      CMP# end1 ; compare to end of string
+      LDA* ptr  ; load value in ptr
+      CMP# 00   ; compare to end of string
       BNE loop  ; loop if not equal
       LDA# answ ; load direct "answer" address to A
       STA ptr   ; store to ptr
@@ -13,24 +15,25 @@ wait: INA       ; get char from input
       BEQ wait  ; wait until input has a value
       STA* ptr  ; indirectly store in answ
       CMP# 0A   ; check for new line
-      BEQ resu  ; print results
+      BEQ prnt  ; print results if done
       LDA ptr   ; load pos
       INC       ; increment to next address location
       STA ptr   ; save pos
       JMP wait  ; get next character
-resu: LDA# resp ; load response ptr to A
+prnt: LDA# 00   ; set A to 00
+      STA* ptr  ; terminate string with 00
+      LDA# resp ; load response ptr to A
       STA ptr   ; store in ptr
-res2: OUT* ptr  ; print response
+res2: LDA ptr   ; load pointer
+      OUT* ptr  ; print response
       INC       ; increment to next address
       STA ptr   ; store new position
-      CMP# end2 ; compare to new line
+      LDA* ptr  ; load value in ptr
+      CMP# 00   ; compare to new line
       BNE res2  ; if not, print next character
       JMP init  ; go again
 ptr:  00        ; Pointer to string
 ques: "What is your name? "
-end1: NOP
+      00
 resp: "Hello, "
-answ: "          "
-      0D
-      0A
-end2: NOP
+answ: 00
